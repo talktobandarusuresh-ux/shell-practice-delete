f7b3528659256ee7cf17ab7ea10c3e39b62ab27f
Suresh Bandaru <bandarucloudmasters@gmail.com>
2025-12-23 13:43:08 +0530

    Shell Practice

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
+echo "Script name: $0"
+echo "Current directory: $PWD"
+echo "Who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script: $$"
+sleep 50 &
+echo "PID of the last command in background is: $!"
