c3ab2f38278e7b99dc69bd4b920e78d6d7ea580f
Suresh Bandaru <bandarucloudmasters@gmail.com>
2025-12-23 12:07:09 +0530

    Shell Practice

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hi Ramesh, I am fine, thank you. How about you?"
+echo "Ramesh:: I am doing well too. Thanks for asking!"
+echo "Suresh:: Great to hear that. Let's catch up sometime soon."
+echo "Ramesh:: Absolutely! Looking forward to it."
