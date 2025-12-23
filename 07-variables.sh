e0f362cd8b50f38eb59972be88156589dd6cc00d
Suresh Bandaru <bandarucloudmasters@gmail.com>
2025-12-23 13:29:20 +0530

    Shell Practice

--- 07-variables.sh
+++ 07-variables.sh
@@ -6,6 +6,7 @@ START_TIME=$(date +%s)
 sleep 10 &
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$($END_TIME-$START_TIME)
+#TOTAL_TIME=$($END_TIME-$START_TIME) Add one more brace like shown in below command
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 
 echo "Script executed in: $TOTAL_TIME seconds"
